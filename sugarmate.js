2ca21e9a868ae7bc7923f20ffdc7756179c74a96
LostInThePines <robertalderson4@hotmail.com>
2021-01-03 02:54:15 -0800

    Update sugarmate.js

--- sugarmate.js
+++ sugarmate.js
@@ -1,7 +1,7 @@
 let sugar = fetch("https://sugarmate.io/api/v1/bhdqxg/latest.json");
 
 if (sugar.ok) {
-  let json = await sugar.json();
+  let json = sugar.json();
   alert(json["value"])
 } else {
   alert("HTTP-Error: " + sugar.status);
