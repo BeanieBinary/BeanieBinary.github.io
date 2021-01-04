6ac210a002a37158204183cbdb9787d77033ef49
LostInThePines <robertalderson4@hotmail.com>
2021-01-03 21:47:35 -0800

    Update sugarmate.js

--- sugarmate.js
+++ sugarmate.js
@@ -1,4 +1,4 @@
-let url = "Access-Control-Allow-Origin: https://sugarmate.io/api/v1/bhdqxg/latest.json"
+let url = "https://sugarmate.io/api/v1/bhdqxg/latest.json"
 let sugar = fetch(url)
 
     .then(response => response.json())
