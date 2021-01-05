77ffa30a6db00d61e16b3a0d4aa12843eee0df93
LostInThePines <robertalderson4@hotmail.com>
2021-01-04 21:44:52 -0800

    Update sugarmate.js

--- sugarmate.js
+++ sugarmate.js
@@ -1,5 +1,5 @@
 fetch('https://sugarmate.io/api/v1/bhdqxg/latest.json', {
   mode:'no-cors'
 })
-    .then(response => response.json())
+    .then(response => response.json.parse())
     .then(data => console.log(data));
