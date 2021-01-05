e7f7fcedae8a658d4fddc12b294aa041fb41986c
LostInThePines <robertalderson4@hotmail.com>
2021-01-04 21:52:09 -0800

    Update sugarmate.js

--- sugarmate.js
+++ sugarmate.js
@@ -1,5 +1,20 @@
-fetch('https://sugarmate.io/api/v1/bhdqxg/latest.json', {
-  mode:'no-cors'
-})
-    .then(response => response.json.parse())
-    .then(data => console.log(data));
+function getJSONP(url, success) {
+
+    var ud = '_' + +new Date,
+        script = document.createElement('script'),
+        head = document.getElementsByTagName('head')[0]
+               || document.documentElement;
+
+    window[ud] = function(data) {
+        head.removeChild(script);
+        success && success(data);
+    };
+
+    script.src = url.replace('callback=?', 'callback=' + ud);
+    head.appendChild(script);
+
+}
+
+getJSONP('https://sugarmate.io/api/v1/bhdqxg/latest.json', function(data){
+    console.log(data);
+});
